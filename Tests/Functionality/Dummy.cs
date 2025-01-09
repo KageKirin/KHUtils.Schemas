de717bb191804a642b3c6a4ac1226fec1a22dd82
Christian Helmich <kagekirin@gmail.com>
2025-01-09 13:21:12 +0900

    added functionality unit tests (dummy) (build/functionality-unit-tests) [#6]
    - **added Functionality.csproj**
    - **implemented dummy unit test**

---
+++ Tests/Functionality/Dummy.cs
@@ -0,0 +1,13 @@
+using KHUtils.Schemas;
+using Xunit;
+
+namespace KHUtils.Schemas.Functionality.Tests;
+
+public class Dummy
+{
+    [Fact]
+    public void Test()
+    {
+        Assert.True(true);
+    }
+}
